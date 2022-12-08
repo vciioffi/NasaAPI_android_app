b131ce846d9e432ec62765f373fea7a3521c2a8c
vciioffi <vciioffi@gmail.com>
2022-12-09 00:10:07 +0100

    feature: get apod usecase created with repository

--- app/src/main/java/com/example/theuniverseapp/apod/data/datasource/ApodDataSource.kt
+++ app/src/main/java/com/example/theuniverseapp/apod/data/datasource/ApodDataSource.kt
@@ -1,5 +1,13 @@
 package com.example.theuniverseapp.apod.data.datasource
 
+import android.content.res.Resources
+import android.provider.Settings.Secure.getString
+import com.example.theuniverseapp.R
+import com.example.theuniverseapp.apod.data.ApodService
+import com.example.theuniverseapp.apod.data.model.ApodDto
+import com.example.theuniverseapp.apod.utils.getRetrofit
+import retrofit2.Response
+
 interface ApodDataSourceInterface{
 
     interface Local{
@@ -10,18 +18,19 @@ interface ApodDataSourceInterface{
 
     }
     interface Remote{
-        suspend fun getApodFromApi(){
-
-        }
+        suspend fun getApodFromApi():Response<ApodDto>
     }
 }
 
+//TODO: Inject retrofit and db and string resource apikey
 class ApodDataSource(): ApodDataSourceInterface.Local,ApodDataSourceInterface.Remote{
     override suspend fun getApodFromDatabase() {
         TODO("Not yet implemented")
     }
-    override suspend fun getApodFromApi() {
-        TODO("Not yet implemented")
+    override suspend fun getApodFromApi():Response<ApodDto> {
+        val retrofit = getRetrofit().create(ApodService::class.java)
+        val apiKey = Resources.getSystem().getString(R.string.api_key)
+       return retrofit.getPictureOfTheDay("apod?api_key=e2B3Gl8ifZIxrBoMzeSdgJJvHxUamnF6MqcB36QM")
     }
 
 
